5ee65ebe04579a0d71bb70659098e327ac537831
Historia Montague <historia.montague@outlook.com>
2023-05-22 14:26:48 -0600

    Finished Lab

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,8 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 1:
+        return items[0]
+    if len(items) == 2:
+        return " and ".join(items)
+    else:
+        items[-1] = "and " + items[-1]
+        return ", ".join(items)
